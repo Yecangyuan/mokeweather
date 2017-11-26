31fe57c951fd95bc9a27a33f411025ab197f597e
Yecangyuan <Yecangyuan@github.com>
2017-11-26 20:16:20 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/mokeweather/android/WeatherActivity.java
+++ app/src/main/java/com/mokeweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.mokeweather.android;
 
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.mokeweather.android.gson.Forecast;
 import com.mokeweather.android.gson.Weather;
+import com.mokeweather.android.service.AutoUpdateService;
 import com.mokeweather.android.util.HttpUtil;
 import com.mokeweather.android.util.Utility;
 
@@ -151,6 +153,7 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather" , responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+
                         }else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",
                                     Toast.LENGTH_SHORT).show();
@@ -205,6 +208,8 @@ public class WeatherActivity extends AppCompatActivity {
         if (weather.aqi != null){
             aqiText.setText(weather.aqi.city.aqi);
             pm25Text.setText(weather.aqi.city.pm25);
+
+
         }
         String comfort = "舒适度："+weather.suggestion.comfort.info;
         String carWash = "洗车指数："+weather.suggestion.carWash.info;
@@ -213,6 +218,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
     /**
      * 加载必应每日一图
